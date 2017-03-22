edf00a2f87a64ca0e725590dca7e37f62fd17c73
Allan Salandanan <Allan@Moniques-MacBook-Pro.local>
2017-03-22 15:26:39 -0700

    updated code

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyA8mdI9pPhTqOPgYyL8XDukH_9WlKHrWHc",
+	authDomain: "train-scheduler-8ee61.firebaseapp.com",
+    databaseURL: "https://train-scheduler-8ee61.firebaseio.com",
+    storageBucket: "train-scheduler-8ee61.appspot.com",
+    messagingSenderId: "738434695326"
+};
+firebase.initializeApp(config);
+
+//on click function
+$("#submit-btn").on("click", function (e) {
+  	e.preventDefault();
+  	//variables to reference firebase database
+	var database = firebase.database();
+	var dataRef = database.ref();
+
+  	//variables to hold values of input boxes
+  	var trainName = $("#train-name").val();
+  	var destination = $("#destination").val();
+  	var firstTrain = $("#first-train").val();
+  	var frequency = $("#frequency").val();
+  	console.log(frequency);
+
+  	dataRef.push({
+  		trainName : trainName,
+  		destination : destination,
+  		firstTrain : firstTrain,
+  		frequency : frequency
+  	});
+
+  	dataRef.on ("child_added", function (trainInfo) {
+  		
+  	})
+});
+
+});
